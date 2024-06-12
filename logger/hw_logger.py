2d82f8bf7f1a2f1fa237f51548b5e862b975049d
Kyungill Jung <jki0113@naver.com>
2024-06-12 17:11:51 +0900

    add setting for a base logger

--- logger/hw_logger.py
+++ logger/hw_logger.py
@@ -1,14 +1,13 @@
 import os
-from dotenv import load_dotenv
 import time
 import psutil
+from threading import Thread
 
 
-load_dotenv('.env.prod')
-HARDWARE_LOGGER_PATH = os.getenv('HARDWARE_LOGGER_PATH')
+
 
 def manage_log_size(log_file, max_lines=3000):
-    """log가 최근 3000줄 까지만 작성되게 해주는 함수"""
+    """log가 최근 max_lines 까지만 작성되게 해주는 함수"""
     with open(log_file, 'r') as file:
         lines = file.readlines()
 
@@ -19,7 +18,7 @@ def manage_log_size(log_file, max_lines=3000):
 
 def write_hw_log():
     """특정 시간마다 HARDWARE_LOGGER_PATH에 로깅"""
-    log_path = os.path.join(HARDWARE_LOGGER_PATH, 'hw.log')
+    log_path = os.path.join(os.getenv('HARDWARE_LOGGER_PATH'), 'hw.log')
     with open(log_path, 'a') as hw_file:
         while True:
             cpu_percent = psutil.cpu_percent(interval=1)
@@ -30,6 +29,10 @@ def write_hw_log():
             hw_file.flush()
             manage_log_size(log_path)
             time.sleep(30)
+
+def start_hw_logger():
+    thread = Thread(target=write_hw_log, daemon=True)
+    thread.start()
 
 
 if __name__ == "__main__":
--- logger/logger_config.py
+++ logger/logger_config.py
@@ -5,26 +5,34 @@ import logging
 import colorlog
 from colorama import Fore, Style
 from functools import wraps
+from datetime import datetime
 
-# 로깅 레벨 및 로깅 파일 저장 위치 설정
-LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL')
-SYSTEM_LOGGER_PATH = os.environ.get('SYSTEM_LOGGER_PATH') #pm2에서 log 경로를 지정해줬기 때문에 필요 없음
+LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL').upper()
+SYSTEM_LOGGER_PATH = os.environ.get('SYSTEM_LOGGER_PATH')
+print(LOGGING_LEVEL)
 
 class ColoredFormatter(colorlog.ColoredFormatter):
     def format(self, record):
         formatted_record = super().format(record)
-        lines = formatted_record.split('\n')
-        first_line = lines[0]
-        log_level_with_color = first_line.split('|')[0]
-        colored_lines = [log_level_with_color + '|' + line if line else line for line in lines[1:]]
-        return '\n'.join([first_line] + colored_lines)
 
-# logger의 설정을 해줌
+        lines = formatted_record.split('\n')
+
+        timestamp = lines[0].split('|')[0].strip()
+        log_level_with_color = lines[0].split('|')[1].strip()
+
+        first_line_content = '|'.join(lines[0].split('|')[2:])[1:]
+
+        colored_lines = [f"{timestamp} | {log_level_with_color} | {first_line_content}"] + [
+            f"{timestamp} | {log_level_with_color} | {line}" for line in lines[1:]
+        ]
+
+        return '\n'.join(colored_lines)
+ 
+# 로거 설정
 logger = logging.getLogger(__name__)
 logger.setLevel(getattr(logging, LOGGING_LEVEL))
 ch = logging.StreamHandler()
 ch.setLevel(getattr(logging, LOGGING_LEVEL))
-
 log_colors = {
     'DEBUG': 'cyan',
     'INFO': 'green',
@@ -33,16 +41,18 @@ log_colors = {
     'CRITICAL': 'white,bg_red',
 }
 
+# 스트림 핸들러에 색상 포맷터를 적용
 ch_formatter = ColoredFormatter(
-    "%(log_color)s%(levelname)s | %(message)s",
-    log_colors=log_colors
+    "%(log_color)s%(asctime)s | %(levelname)s | %(message)s",
+    log_colors=log_colors,
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 ch.setFormatter(ch_formatter)
 logger.addHandler(ch)
 logger.propagate = False
 
+# 비동기 함수의 실행 시간을 로깅하는 데코레이터
 def log_execution_time(func):
-    """ 비동기 함수에 대해 실행 시간을 로깅하는 데코레이터 """
     @wraps(func)
     async def wrapper(*args, **kwargs):
         log('green', f"{func.__name__} | called")
@@ -57,19 +67,14 @@ def log_execution_time(func):
         return result
     return wrapper
 
+# 컬러 로그 메시지를 출력하는 함수
 def log(color, text):
-    """Log function that applies color to each line of a multi-line message."""
     color_prefix = f'{Style.BRIGHT}{getattr(Fore, color.upper())}'
     color_suffix = f'{Style.RESET_ALL}'
-    lines = str(text).split('\n') # Split the text into lines
-    colored_lines = [f"{color_prefix}{line}{color_suffix}" for line in lines] # Apply color to each line
-    colored_message = '\n'.join(colored_lines) # Rejoin the colored lines
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    lines = str(text).split('\n')
+    # 각 줄에 색상을 적용하고 현재 시간을 추가합니다.
+    colored_lines = [f"{color_prefix}{timestamp} | {line}{color_suffix}" for line in lines]
+    # 색상이 적용된 메시지를 다시 결합합니다.
+    colored_message = '\n'.join(colored_lines)
     print(colored_message)
-
-def test_function():
-    logger.debug("This is a debug message with multiple lines")
-    logger.info("This is an info message with multiple lines")
-    logger.warning("This is a warning message with multiple lines")
-    logger.error("This is an error message with multiple lines")
-    logger.critical("This is a critical message with multiple lines")
-test_function()
